23230d544bc2a47de675479b1e8160eb45310621
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-03-20 15:18:34 +0200

    Add files via upload

---
+++ 06,02/06,02/06,02.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+template <typename T>
+class Swapper {
+public:
+    void Swap(T& a, T& b) {
+        T temp = a;
+        a = b;
+        b = temp;
+    }
+};
+
+int main() {
+    Swapper<int> intSwapper;
+    int x = 5, y = 10;
+    std::cout << "Before swapping: x = " << x << ", y = " << y << std::endl;
+    intSwapper.Swap(x, y);
+    std::cout << "After swapping: x = " << x << ", y = " << y << std::endl;
+
+    Swapper<double> doubleSwapper;
+    double a = 3.14, b = 2.71;
+    std::cout << "Before swapping: a = " << a << ", b = " << b << std::endl;
+    doubleSwapper.Swap(a, b);
+    std::cout << "After swapping: a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
